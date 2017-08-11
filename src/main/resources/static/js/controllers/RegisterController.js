948c424547b6aec86ca16a515d42f7be748510ad
dominika.sporzynska@gmail.com <2002sdsd>
2017-08-11 13:19:22 +0200

    rejestrowania poprawnosc wprowadzanych danych

--- src/main/resources/static/js/controllers/RegisterController.js
+++ src/main/resources/static/js/controllers/RegisterController.js
@@ -1,6 +1,119 @@
 angular.module('myApp').controller('RegisterController', function ($scope, $resource, $http) {
     $scope.message = '';
 
+    var n=false;
+    var ln=false;
+    var p=false;
+    var e=false;
+    $('.sendButton').attr('disabled', true);
+
+
+    // do  walidania do nazwy usera
+    $(document).ready(function () {
+     //   $('.sendButton').attr('disabled', true);
+
+        $('#imie').keyup(function () {
+            if ($(this).val().length >= 3) {
+                // $('.sendButton').attr('disabled', false);
+                n=true;
+                $scope.CorrectData();
+            }
+            else {
+                n=false;
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', true);
+            }
+        })
+    });
+    // do  walidania do nazwy usera
+    $(document).ready(function () {
+      //  $('.sendButton').attr('disabled', true);
+
+        $('#nazwisko').keyup(function () {
+
+                $('.sendButton').attr('disabled', true);
+
+
+            if ($(this).val().length >= 3) {
+                ln=true;
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', false);
+            }
+            else {
+                ln=false;
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', true);
+            }
+        })
+    });
+
+// do hasła
+    $(document).ready(function () {
+        $('.sendButton').attr('disabled', true);
+
+        $('#password').keyup(function () {
+
+
+            if ($(this).val().length >= 5) {
+                p=true;
+                console.log(p);
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', false);
+            }
+            else {
+                p=false;
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', true);
+            }
+        })
+    });
+
+
+// do maila
+    $(document).ready(function () {
+        $('.sendButton').attr('disabled', true);
+
+        $('#email').keyup(function () {
+
+
+            if ($(this).val().length >= 4) {
+                e=true;
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', false);
+            }
+            else {
+                e=false;
+              
+                $scope.CorrectData();
+                // $('.sendButton').attr('disabled', true);
+            }
+        })
+    });
+
+$scope.CorrectData = function () {
+
+
+
+
+
+
+    if (n==true && ln==true && p==true && e==true ) {
+
+        $('.sendButton').attr('disabled', false);
+        console.log(n==true & ln==true & p==true & e==true);
+    }
+    else {
+        console.log(n==true & ln==true & p==true & e==true);
+        $('.sendButton').attr('disabled', true);
+    }
+};$scope.CorrectData();
+
+
+
+
+
+
+
     $scope.test = function() {
         alert('Thanks');
     }
@@ -9,6 +122,7 @@ angular.module('myApp').controller('RegisterController', function ($scope, $reso
     });
 
     $scope.saveUser= function () {
+
         var email = $scope.emailOfUser;
         var firstName = $scope.firstNameOfUser; //pobieramy imie z pola w html
         var lastName = $scope.lastNameOfUser;
