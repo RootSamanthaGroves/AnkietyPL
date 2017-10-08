f36a3d939092c146a0ea852e791ac50e92ce6ccd
dominika.sporzynska@gmail.com <2002sdsd>
2017-10-08 17:10:49 +0200

    wizualizacja regul 6 poprawa potrojnych poprzednikow

--- src/main/java/com/ankietypl/repository/Init.java
+++ src/main/java/com/ankietypl/repository/Init.java
@@ -5,7 +5,9 @@ package com.ankietypl.repository;
  */
 
 
+import com.ankietypl.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.JpaSort;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -16,31 +18,37 @@ import javax.annotation.PostConstruct;
  * Created by DiiES on 2017-06-05.
  */
 
-import com.ankietypl.model.Role;
-import com.ankietypl.model.User;
+import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.lang.reflect.Array;
+import java.net.URL;
+import java.util.*;
 
 
-/**
- * Created by kosa1010 on 04.01.17.
- */
 @Component
 public class Init {
 
     @Autowired
     UserRepository userRepository;
-
-
+    @Autowired
+    SurveyRepository surveyRepository;
+    @Autowired
+    AnswerRepository answerRepository;
+    @Autowired
+    QuestionRepository questionRepository;
 
     @PostConstruct
-    public void init() {
+    public void init() throws IOException {
         User user1 = userRepository.findOneByEmail("user@o2.pl");
         User admin = userRepository.findOneByEmail("admin@o2.pl");
         if (user1 == null) {
             User user = new User();
             user.setEmail("user@o2.pl");
             user.setFirstName("User");
+            user.setLastName("User");
             BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            user.setPassword(encoder.encode("admin"));
+            user.setPassword(encoder.encode("user123"));
             user.setRole(Role.ROLE_USER);
             userRepository.save(user);
         }
@@ -56,6 +64,162 @@ public class Init {
         }
 
 
+        Survey s = surveyRepository.findOne(1);
+        if (s == null) {
+            Survey survey = new Survey();
+            survey.setTitle("Negatywne aspekty komputeryzacji w życiu współczesnego człowieka");
+            //survey.setQuestion();
+            surveyRepository.save(survey);
+
+            System.out.println("DZIAłA");
+            File file = new File("./src/main/resources/static/data/danedobazy/answers.csv");
+            System.out.println("nadal działa");
+            Scanner in = new Scanner(file);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            Answer answer;
+            String q = "";
+//            try {
+//                String textLine = bufferedReader.readLine();
+//                while (textLine != null && textLine.length() > 0) {
+//                    int index1 = textLine.indexOf(",");
+//                    q = textLine.substring(index1 + 1);
+//                    System.out.println(q);
+//                    answer = new Answer(q);
+//                    answerRepository.save(answer);
+//                    textLine = bufferedReader.readLine();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    bufferedReader.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//ffffffffffffffffffffffffffffffffffffffff
+
+//            file = new File("./src/main/resources/static/data/danedobazy/question.csv");
+//
+//            in = new Scanner(file);
+//            fileReader = new FileReader(file);
+//            bufferedReader = new BufferedReader(fileReader);
+//            Question question;
+//            ArrayList<Question> listOfQuestion = new ArrayList<Question>();
+//
+//            q = "";
+//         //   String k="";
+//            try {
+//                String textLine = bufferedReader.readLine();
+//                while (textLine != null && textLine.length() > 0) {
+//                    int index1 = textLine.indexOf(",");
+//                    int index2 = textLine.lastIndexOf(",");
+//                    q = textLine.substring(index1 + 1);
+//                 //   k= textLine.substring(index2,textLine.length());
+//                   System.out.println(q);
+//                    question = new Question(q);
+//                    questionRepository.save(question);
+//                    listOfQuestion.add(question);
+//
+//                    textLine = bufferedReader.readLine();
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } finally {
+//                try {
+//                    bufferedReader.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+
+            String csvFile = "./src/main/resources/static/data/danedobazy/questionanswer.csv";
+            BufferedReader br = null;
+            String line;
+            String cvsSplitBy = ",";
+
+            try {
+
+                br = new BufferedReader(new FileReader(csvFile));
+                while ((line = br.readLine()) != null) {
+
+                    // use comma as separator
+                    String[] country = line.split(cvsSplitBy);
+
+                    System.out.println("Country [code= " + country[0] + " , name=" + country[1] + "]");
+
+                }
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+
+//            System.out.println(new File());
+            file = new File("./src/main/resources/static/data/danedobazy/questionanswer.csv");
+
+//             in = new Scanner(file);
+            fileReader = new FileReader(file);
+            bufferedReader = new BufferedReader(fileReader);
+
+
+            Answer answer1;
+
+            ArrayList<Answer> listOfAnswer = new ArrayList<Answer>();
+
+            q = "";
+            String id = "";
+            try {
+                String textLine = bufferedReader.readLine();
+                while (textLine != null && textLine.length() > 0) {
+                    int index1 = textLine.indexOf(",");
+//                    q = textLine.substring(index1 + 1);
+//                    id = textLine.substring(0,index1) ;
+//                   System.out.println(q+" " +id);
+//                    question = new Question(q);
+//                    answerRepository.save();
+//                    listOfAnswer.add(answer);
+                    System.out.println(textLine);
+                    textLine = bufferedReader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                bufferedReader.close();
+            }
+
+
+//            survey.setQuestion(listOfQuestion);
+//            surveyRepository.update(1, survey);
+
+        }
     }
+
+
+    public void listFilesForFolder(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listFilesForFolder(fileEntry);
+            } else {
+                System.out.println(fileEntry.getName());
+            }
+        }
+    }
+
+
 }
+
+
+
+
 
